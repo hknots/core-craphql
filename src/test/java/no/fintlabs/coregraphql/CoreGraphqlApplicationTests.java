7f1a4e6ab6330eb8eaef72ba569349685c071c40
Hknots <Hknots99@gmail.com>
2023-11-21 14:23:00 +0100

    initial commit

---
+++ src/test/java/no/fintlabs/coregraphql/CoreGraphqlApplicationTests.java
@@ -0,0 +1,13 @@
+package no.fintlabs.coregraphql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreGraphqlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
