b80f821525f000049c3393b2d2ec2922cb258509
Hknots <Hknots99@gmail.com>
2023-11-28 12:09:49 +0100

    add recursive method to create types

--- src/main/java/no/fintlabs/coregraphql/config/QueryConfig.java
+++ src/main/java/no/fintlabs/coregraphql/config/QueryConfig.java
@@ -5,25 +5,27 @@ import graphql.schema.GraphQLArgument;
 import graphql.schema.GraphQLFieldDefinition;
 import graphql.schema.GraphQLObjectType;
 import lombok.RequiredArgsConstructor;
-import no.fintlabs.coregraphql.reflection.FintClass;
+import lombok.extern.slf4j.Slf4j;
+import no.fintlabs.coregraphql.reflection.model.FintMainObject;
 import no.fintlabs.coregraphql.reflection.ReflectionService;
+import no.fintlabs.coregraphql.reflection.model.FintObject;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 import java.util.stream.Collectors;
 
+@Slf4j
 @Configuration
 @RequiredArgsConstructor
 public class QueryConfig {
 
+    private final Map<String, GraphQLObjectType> procsessedTypes = new HashMap<>();
     private final ReflectionService reflectionService;
 
     @Bean
     public GraphQLObjectType buildQueryType() {
-        List<GraphQLFieldDefinition> fieldDefinitions = reflectionService.getFintClasses().values().stream()
+        List<GraphQLFieldDefinition> fieldDefinitions = reflectionService.getFintMainObjects().values().stream()
                 .map(this::buildFieldDefinition)
                 .collect(Collectors.toList());
 
@@ -33,25 +35,54 @@ public class QueryConfig {
                 .build();
     }
 
-    private GraphQLFieldDefinition buildFieldDefinition(FintClass fintClass) {
-        GraphQLObjectType.Builder objectTypeBuilder = GraphQLObjectType.newObject()
-                .name(fintClass.getClazz().getSimpleName());
-
-        // Define fields for the object type
-        // Assuming all fields are of type String for simplicity
-        fintClass.getIdentifikatorFields().forEach(field ->
-                objectTypeBuilder.field(GraphQLFieldDefinition.newFieldDefinition()
-                        .name(field)
-                        .type(Scalars.GraphQLString)
-                        .build()));
-
-        GraphQLObjectType objectType = objectTypeBuilder.build();
-
+    private GraphQLFieldDefinition buildFieldDefinition(FintMainObject fintMainObject) {
         return GraphQLFieldDefinition.newFieldDefinition()
-                .name(fintClass.getClazz().getSimpleName().toLowerCase())
-                .arguments(buildArguments(fintClass.getIdentifikatorFields()))
-                .type(objectType)
+                .name(fintMainObject.getClazz().getSimpleName().toLowerCase())
+                .arguments(buildArguments(fintMainObject.getIdentifikatorFields()))
+                .type(createObjectType(fintMainObject))
                 .build();
+    }
+
+    private GraphQLObjectType createObjectType(FintObject fintObject) {
+        if (typeIsProcessed(fintObject.getClazzSimpleName())) {
+            return procsessedTypes.get(fintObject.getClazzSimpleName());
+        }
+        log.info("Creating object: {}", fintObject.getClazzSimpleName());
+        GraphQLObjectType.Builder objectBuilder = GraphQLObjectType.newObject()
+                .name(fintObject.getClazzSimpleName());
+
+        fintObject.getFields().forEach(field -> {
+            GraphQLFieldDefinition.Builder fieldBuilder = GraphQLFieldDefinition.newFieldDefinition()
+                    .name(field.getName());
+
+            if (typeIsProcessed(field.getType().getSimpleName())) {
+                log.info("Type: {} - {} has already been processed", field.getType().getSimpleName(), field.getName());
+                objectBuilder.field(fieldBuilder.type(procsessedTypes.get(field.getType().getSimpleName())).build());
+            } else if (typeIsFromJava(field.getType())) {
+                log.info("Type: {} - {} is from java", field.getType().getSimpleName(), field.getName());
+                objectBuilder.field(fieldBuilder.type(Scalars.GraphQLString).build());
+            } else {
+                log.warn("Type {} is not processed", field.getType().getSimpleName());
+                objectBuilder.field(fieldBuilder.type(createNewType(field.getType().getSimpleName())).build());
+            }
+
+            log.info("Type: {} - {} is created", field.getType().getSimpleName(), field.getName());
+        });
+        GraphQLObjectType build = objectBuilder.build();
+        procsessedTypes.put(fintObject.getClazzSimpleName(), build);
+        return build;
+
+    }
+
+    private GraphQLObjectType createNewType(String typeName) {
+        if (reflectionService.getFintMainObjects().containsKey(typeName)) {
+            return createObjectType(reflectionService.getFintMainObjects().get(typeName));
+        } else if (reflectionService.getFintComplexObjects().containsKey(typeName)) {
+            return createObjectType(reflectionService.getFintComplexObjects().get(typeName));
+        } else {
+            log.error("Type {} not found", typeName);
+            return null;
+        }
     }
 
     private List<GraphQLArgument> buildArguments(Set<String> identifikatorFields) {
@@ -62,6 +93,24 @@ public class QueryConfig {
                         .type(Scalars.GraphQLString)
                         .build()));
         return arguments;
+    }
+
+    private boolean typeIsFromJava(Class<?> clazz) {
+        if (clazz.getClassLoader() == null) {
+            return true;
+        }
+
+        Package classPackage = clazz.getPackage();
+        if (classPackage != null) {
+            String packageName = classPackage.getName();
+            return packageName.startsWith("java") || packageName.startsWith("javax");
+        }
+
+        return false;
+    }
+
+    private boolean typeIsProcessed(String typeName) {
+        return procsessedTypes.containsKey(typeName);
     }
 
 }
